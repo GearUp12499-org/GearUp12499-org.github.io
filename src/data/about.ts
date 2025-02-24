e4d47364764567f4e1da5b6bf2478ed13c7d7183
shuban-789 <cybergeek11929@gmail.com>
2025-02-23 17:05:31 -0800

    e

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,6 +2,6 @@ export const about = {
     title: "About Us",
     subtitle: "",
     description:
-      "12499 Gear Up is a world-class robotics high school team based in San Diego, California. Every year, we design, build, and program a robot to compete in the FIRST Tech Challenge (FTC) competition. Additionally, we also dedicate our time to outreach and spreading the message of FIRST and STEM to our community through the development of various events, workshops, and technical projects.",
+      "12499 Gear Up is a world-class high school robotics team based in San Diego, California. Every year, we design, build, and program a robot to compete in the FIRST Tech Challenge (FTC) competition. Additionally, we also dedicate our time to outreach and spreading the message of FIRST and STEM to our community through the development of various events, workshops, and technical projects.",
   };
   
