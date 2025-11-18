3d8cc8c20b4950c9802eda56a802d838d6e2c70f
shuban-789 <cybergeek11929@gmail.com>
2025-11-17 16:55:52 -0800

    new gallery

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,6 +1,17 @@
 export const gallery = {
     title: "Gallery",
-    subtitle: "",
-    description:
-      "Coming soon to a 12499 Gear Up website near you!",
-  };
+    images: [
+        { src: "/images/decodeHello.jpg", alt: "time for DECODE !!" },
+        { src: "/images/sdftcRegional.jpg", alt: "SD Regional 2025" },
+        { src: "/images/fireRobotRender.PNG", alt: "stingray!!" },
+        { src: "/images/ucsdGeniosity.jpg", alt: "GearUp @ UCSD" },
+        { src: "/images/posterBear.jpg", alt: "studious bear" },
+        { src: "/images/bearHang.jpg", alt: "Djungelhang" },
+        { src: "/images/goatedMatch.jpg", alt: "goated teamwork" },
+        { src: "/images/gearUpWin.jpg", alt: "goated gauss win" },
+        { src: "/images/blahajCart.jpg", alt: "cart of blahaj" },
+        { src: "/images/bearOnBasket.jpeg", alt: "bear on basket" },
+        { src: "/images/bulletBear.jpg", alt: "bear nerf war" },
+        { src: "/images/kingBear.jpg", alt: "bear ruling field" },
+    ]
+};
